1cb6d273994e6239bd462e4fd005a1ea440909aa
Shyam Sharki <ssk66221@gmail.com>
2024-12-17 07:58:18 +0545

    closures

---
+++ Variable & Scope & Closures/Example1.js
@@ -0,0 +1,8 @@
+function sayHello(firstName,lastName){
+    function getFullName(){
+        return firstName + " " + lastName;
+    }
+    console.log(getFullName());
+
+}
+sayHello("Radha","Sarki");
---
+++ Variable & Scope & Closures/Example2.js
@@ -0,0 +1,10 @@
+function makeCounter(){
+    let count=0;
+    return function(){
+        return count++;
+    }
+}
+
+let counter=makeCounter();
+console.log(counter());
+console.log(counter());
